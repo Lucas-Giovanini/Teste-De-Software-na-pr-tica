58e8d8b82479a9ab7cfc1189632c36605acfffa8
lminervino <lminervino@uea.edu.br>
2026-03-03 13:17:58 +0000

    missao cumprida

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,13 @@
 { pkgs, ... }: {
-  # Canal do repositório de pacotes
   channel = "stable-23.11";
-
-  # Pacotes que serão instalados no ambiente
-  packages = [
-    pkgs.nodejs_20
-    pkgs.eslint
+  packages = [ 
+    pkgs.nodejs_20 
+    pkgs.eslint 
+    pkgs.nodePackages.firebase-tools # Adicione isso para ter o comando 'firebase'
   ];
-
-  # Extensões do VS Code/IDX
-  idx = {
-    extensions = [
-      "dbaeumer.vscode-eslint"    # Mostra os avisos de linting
-      "usernamehw.errorlens"      # Exibe mensagens de erro direto na linha
-      "esbenp.prettier-vscode"    # Formatador de código
-    ];
-    
-    # Opcional: Ativar a formatação automática ao salvar
-    previews = {
-      enable = true;
-    };
-  };
+  idx.extensions = [
+    "dbaeumer.vscode-eslint"
+    "usernamehw.errorlens"
+    "esbenp.prettier-vscode"
+  ];
 }
